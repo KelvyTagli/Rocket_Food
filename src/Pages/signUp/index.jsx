b9fe2a31295c98af515d46e61894275a41b7b23c
Kelvy Tagli <kelvytagliacolli@hotmail.com.br>
2024-03-10 19:01:24 -0300

    feat: cadastro de usuario

--- src/Pages/signUp/index.jsx
+++ src/Pages/signUp/index.jsx
@@ -4,9 +4,36 @@ import background from '../../assets/Polygon.svg'
 
 import { Input } from "../../Components/Input";
 import { Button } from "../../Components/Button";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+import { api } from '../../Services/api'
 
 
 export function SignUp() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [admin, setAdmin] = useState("")
+
+    const navigate = useNavigate()
+
+    function handlerSignUp() {
+        if(!name || !email || !password) {
+            return alert('preencha todos os campos!')
+        }
+
+        api.post("/users", {name, email, password, admin}).then(() =>{
+            alert('Usuário cadastrado com sucesso!')
+            navigate(-1)
+        }).catch( error => {
+            if(error.response) {
+                alert(error.response.data.message)
+            } else {
+                alert('Não foi possivel cadastrar')
+            }
+        })
+    }
     return(
         <Container>
             <Background>
@@ -19,8 +46,10 @@ export function SignUp() {
                 <Input title="Email" placeholder="Exemplo: exemplo@exemplo.com.br" type="text" onChange={e => setEmail(e.target.value)}/>
                 <Input title="Senha" placeholder="No minimo 6 caracteres" type="password" onChange={e => setPassword(e.target.value)}/>
 
-                <Button title={"Criar conta"}/>
-                <a href='/'>Já tenho uma conta</a>
+                <Button title={"Criar conta"} onClick={handlerSignUp}/>
+                <Link to='/'>
+                    Já tenho uma conta
+                </Link>
             </Form>
         </Container>
     )
